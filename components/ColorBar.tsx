784ba8a380965a8421faaecf111487a08fc29c8f
Dave Keller <davekeller@me.com>
2025-11-20 01:00:11 -0600

    feat: accomplishments-inspired color palette and more visible color bar

--- components/ColorBar.tsx
+++ components/ColorBar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ColorBar = () => {
   return (
-    <div className="fixed top-0 z-50 flex h-[8px] w-full justify-evenly bg-black/50 backdrop-blur-sm">
+    <div className="fixed top-0 z-50 flex h-[12px] w-full justify-evenly">
       <div className="w-[10%] bg-[var(--color-1)] shadow-[0_0_10px_var(--color-1)]"></div>
       <div className="w-[10%] bg-[var(--color-2)] shadow-[0_0_10px_var(--color-2)]"></div>
       <div className="w-[10%] bg-[var(--color-3)] shadow-[0_0_10px_var(--color-3)]"></div>
